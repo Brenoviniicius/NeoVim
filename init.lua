67feb7511d30e7cd98cf234a792d02c4dec65941
Breno <brenovmsilva82@gmail.com>
2025-04-21 18:01:52 -0300

    feat: Adicionando o gerenciador de pacotes Lazy

--- init.lua
+++ init.lua
@@ -2,3 +2,16 @@ vim.cmd("set expandtab") -- Faz com que o tab adicione espaços ao invés de um 
 vim.cmd("set tabstop=2") -- Tab irá contar como dois espaços
 vim.cmd("set softtabstop=2") -- Usa dois espaços ao pressionar Tab no modo de inserção
 vim.cmd("set shiftwidth=2") -- Usará dois espaços para identação
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
