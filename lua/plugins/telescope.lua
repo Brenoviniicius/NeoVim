ff37203aafaaedd670e4c427ad05312d4359fa16
Breno <brenovmsilva82@gmail.com>
2025-04-23 11:59:22 -0300

    refactor:Atualizando o 'neo-tree' para o novo diretorio

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,7 @@
     config = function ()
       local builtin = require("telescope.builtin")
       vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-      vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
    end,
 }
 
