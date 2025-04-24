2f3ea2aaf5512cc2ed7bcd6f154037c24bf1c88e
Breno <brenovmsilva82@gmail.com>
2025-04-23 21:55:03 -0300

    feat:Adicionando o lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,9 +1,10 @@
 return {
   "nvim-lualine/lualine.nvim",
   config = function()
-    options = {
-      theme = 'dracula'
-  }
-  end
+    require('lualine').setup({
+      options = { 
+        theme = 'dracula' 
+      } 
+    })
+  end 
 }
-
