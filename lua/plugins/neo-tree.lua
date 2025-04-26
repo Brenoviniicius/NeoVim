455cbd559cee79c19c6fbbac65f8c292ef5a947b
Breno <brenovmsilva82@gmail.com>
2025-04-25 21:43:55 -0300

    feat:Adicionando o configo do 'neo-tree' para revelar arquivos ocultos

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,15 @@ return {
       "MunifTanjim/nui.nvim",
     },
   config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,          -- mostra os arquivos ocultos
+          hide_dotfiles = false,  -- não esconde arquivos que começam com "."
+          hide_gitignored = false -- mostra arquivos ignorados pelo Git também
+        }
+      }
+    })
       vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
   end
 }
